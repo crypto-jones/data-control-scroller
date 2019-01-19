4ba4242dde3430d89f8f494b0558ea7b5e02e873
crypto-jones <integral.being@gmail.com>
2019-01-19 12:48:39 -0800

    Set up actions

---
+++ client/src/actions/index.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+export const RETRIEVING_ITEMS_LIST = 'RETRIEVING_ITEMS_LIST';
+export const RETRIEVED_ITEMS_LIST = 'RETRIEVED_ITEMS_LIST';
+export const RETRIEVING_ITEMS_DESCRIPTION = 'RETRIEVING_ITEMS_DESCRIPTION';
+export const RETRIEVED_ITEMS_DESCRIPTION = 'RETRIEVED_ITEMS_DESCRIPTION';
+export const ERROR = 'ERROR';
+
+const listRoute =
+  'https://s3­us­west­2.amazonaws.com/frontend.codex/challenge2/items_list.json';
+
+const descriptionRoute =
+  'https://s3­us­west­2.amazonaws.com/frontend.codex/challenge2/items_description.json';
+
+export const getList = () => {
+  const promise = axios.get(listRoute);
+  return dispatch => {
+    dispatch({ type: RETRIEVING_ITEMS_LIST });
+    promise
+      .then(res => dispatch({ type: RETRIEVED_ITEMS_LIST, payload: res.data }))
+      .catch(err => dispatch({ type: ERROR, payload: err }));
+  };
+};
+
+export const getDescription = () => {
+  const promise = axios.get(descriptionRoute);
+  return dispatch => {
+    dispatch({ type: RETRIEVING_ITEMS_DESCRIPTION });
+    promise
+      .then(res =>
+        dispatch({ type: RETRIEVED_ITEMS_DESCRIPTION, payload: res.data })
+      )
+      .catch(err => dispatch({ type: ERROR, payload: err }));
+  };
+};
