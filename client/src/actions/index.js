33c14b5a34f4920415174da33f50aa4b0207c478
crypto-jones <integral.being@gmail.com>
2019-01-20 23:44:31 -0800

    update env var names

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -7,7 +7,7 @@ export const RETRIEVED_ITEMS_DESCRIPTION = 'RETRIEVED_ITEMS_DESCRIPTION';
 export const ERROR = 'ERROR';
 
 export const getList = () => {
-  const path = process.env.LIST_ROUTE || 'http://localhost:5000/list';
+  const path = process.env.REACT_APP_LIST_ROUTE || 'http://localhost:5000/list';
   const promise = axios.get(path);
   return dispatch => {
     dispatch({ type: RETRIEVING_ITEMS_LIST });
@@ -20,7 +20,8 @@ export const getList = () => {
 };
 
 export const getDescription = () => {
-  const path = process.env.DETAILS_ROUTE || 'http://localhost:5000/details';
+  const path =
+    process.env.REACT_APP_DETAILS_ROUTE || 'http://localhost:5000/details';
   const promise = axios.get(path);
   return dispatch => {
     dispatch({ type: RETRIEVING_ITEMS_DESCRIPTION });
